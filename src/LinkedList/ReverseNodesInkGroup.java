16171b18b0c279cbbe9cf598099131f221ba1d6c
shixi_liute <LiuTe120040>
2017-07-08 02:34:10 +0800

    add som code of ListNode

--- src/LinkedList/ReverseNodesInkGroup.java
+++ src/LinkedList/ReverseNodesInkGroup.java
@@ -6,6 +6,42 @@ public class ReverseNodesInkGroup {
 		// TODO Auto-generated method stub 
 
 	}
+	
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(k==1) return head;
+        if(head==null) return head;
+        int index = 1;
+        ListNode end = head;
+        while(index<k && end!=null) {
+        	end = end.next;
+        	index++;
+        }
+        if(end!=null && end.next!=null) {
+        	ListNode tmp = end.next;
+        	end.next = null;
+        	ListNode node = reverse(head);
+        	head.next = reverseKGroup(tmp, k);
+        	return node;
+        }
+        if(end==null) return head;
+        return reverse(head);
+    }
+	
+	
+	public ListNode reverse(ListNode head) {
+		if(head==null || head.next==null) return head;
+		ListNode pre = head;
+		ListNode cur = pre.next;
+		pre.next = null;
+		ListNode next = null;
+		while(cur!=null) {
+			next = cur.next;
+			cur.next = pre;
+			pre = cur;
+			cur = next;
+		}
+		return pre;
+	}
 
 }
 	
